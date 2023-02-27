b9dbe084d2228123fb64d3c420a48c0bafc86268
Amaury258 <amaury.demarque3@etu.univ-lorraine.fr>
2023-02-27 15:10:15 +0100

    Ajout de l'idée de base pour l'application

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,14 @@
+/*
+ * USB Analysor
+ * Version 1.0
+ * Par: Amaury DEMARQUE
+ *      Stagiaire à l'École Nationale de Police de Montbéliard
+ */
+
 import javax.management.*;
+
+import Exceptions.*;
+
 import java.lang.management.ManagementFactory;
 import java.util.*;
 
@@ -6,31 +16,38 @@ public class Main {
     /**
      * Prendre la liste des périphériques usb connectés au pc
      * controler si le périphérique est sur la base des périphériques autorisés
+     * trois cas: 
+     *         la base des périphériques autorisés est une bdd -> requête sql
+     *         la base des périphériques autorisés est une liste globale (pour toute machine) -> trouver la liste
+     *         la base des périphériques autorisés est une liste faite par l'entreprise -> demander la liste
      * si oui, envoyer le périphérique à l'anti-virus
      * si non, ????
-     * 
-     * @param args
      */
     public static void main(String[] args) {
         try {
+            //on prend la liste des périphériques usb connectés au pc
             MBeanServerConnection mbsc = ManagementFactory.getPlatformMBeanServer();
             Set<ObjectName> objs = mbsc.queryNames(new ObjectName("javax.usb.*:*"), null);
-            if(objs.isEmpty()){
-                throw new Exception("Aucun périphérique USB détecté");
-            }
-            for (ObjectName obj : objs) {
-                System.out.println("Périphérique USB détecté : " + obj.getCanonicalName());
-                if(obj.getCanonicalName().contains("vid=0x1a86&pid=0x7523")){
-                    System.out.println("Périphérique USB autorisé");
-                    //envoyer l'objet à l'anti-virus
-                    /*if(antivirus.detecteVirus(obj)){
-                        throw new VirusDetecte("Virus détecté");
-                    } else {
-                        System.out.println("Aucun virus détecté");
-                    }*/
 
-                }else{
-                    throw new NonAutoriseException("Périphérique USB non autorisé");
+            //on vérifie si la liste est vide
+            if(objs.isEmpty()){
+                throw new AucunPeripheriqueException("Aucun périphérique USB détecté");
+            } else {
+                for (ObjectName obj : objs) {
+                    System.out.println("Périphérique USB détecté : " + obj.getCanonicalName());
+                    if(obj.getCanonicalName().contains("vid=0x1a86&pid=0x7523")){
+                        System.out.println("Périphérique USB autorisé");
+                        //envoyer l'objet à l'anti-virus
+                        /*if(antivirus.detecteVirus(obj)){
+                            throw new VirusDetecte("Virus détecté");
+                        } else {
+                            System.out.println("Aucun virus détecté");
+                            lancer le piti programme qui nous demande ce qu'on veux faire (ouvrir dans l'explorateur, etc...)
+                        }*/
+
+                    }else{
+                        throw new NonAutoriseException("Périphérique USB non autorisé");
+                    }
                 }
             }
         } catch (Exception e) {
