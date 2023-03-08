6c771e6a5025411ca38b1525edbbb068bb85816e
Amaury258 <amaury.demarque3@etu.univ-lorraine.fr>
2023-03-08 15:25:28 +0100

    Ajout d'un deuxieme Main pour ici creer à nouveau un tag, le déchiffrer et comparer les deux, si le tag déchiffré est différent du tag créer depuis ce main, la clé est éjecté (ici elle est éjecté dans tous les cas pour le test)

--- src/v2/Main2.java
+++ src/v2/Main2.java
@@ -1,174 +1,84 @@
 package v2;
 
-import com.ibm.icu.text.CharsetDetector;
-import com.ibm.icu.text.CharsetMatch;
-
 import javax.swing.filechooser.FileSystemView;
-import java.io.*;
-import java.nio.charset.Charset;
-import java.text.SimpleDateFormat;
-import java.util.*;
-
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class Main2 {
-
-    public static void main(String[] args) throws IOException {
-        SimpleDateFormat sdf = new SimpleDateFormat("YYYY-MM-DD HH:MM:ss");
-        Scanner sc = new Scanner(System.in);
-        File[] roots = File.listRoots();
-        HashMap<String, File> usb_connected = new HashMap<>();
+    public static void main(String[] args) throws Exception {
         int i = 0;
-        while (true) {
-            FileSystemView fsv = FileSystemView.getFileSystemView();
-            if(i==0){
+        while(true){
+            if (i == 0) {
                 System.out.println("Recherche de clé USB");
                 i++;
             }
+
+            Scanner sc = new Scanner(System.in);
+            FileSystemView fsv = FileSystemView.getFileSystemView();
             File[] newRoots = File.listRoots();
+            //parcours des disques
             for (File root : newRoots) {
-                if (fsv.isDrive(root) && fsv.getSystemTypeDescription(root).contains("USB") && !usb_connected.containsValue(root)) {
+                if (fsv.isDrive(root) && fsv.getSystemTypeDescription(root).contains("USB")){
                     // la clé USB a été trouvée, faites quelque chose ici
                     System.out.println("Clé USB détecté : " + root.getAbsolutePath());
-                    usb_connected.put(root.getAbsolutePath(), root);
-                    System.out.println("Appuyez sur \"Entrée\" pour éjecter la clé USB.");
-                    boolean index = false;
-                    boolean last_modified = false;
-                    boolean hashcode = false;
-                    try {
-                        /*
-                            ->IndexerVolumeGuid
-                            ->lastModified()
-                            ->HashCode
-                         */
-                        File TAG = new File(root.getAbsolutePath()+"System Volume Information\\TAG");
-                        List<String> lines = new ArrayList<>();
-                        if(!TAG.exists()){
-                            FileWriter fw = new FileWriter(TAG,true);
-                            fw.write("IndexerVolumeGuid : \n");
-                            fw.write("Last Modified (long) : \n");
-                            fw.write("HashCode : ");
-                            fw.close();
-                        }
-                        Scanner scan = new Scanner(TAG);
-                        while(scan.hasNextLine()){
-                            String line = scan.nextLine();
-                            if(line.contains("Last Modified (long) : ")){
-                                String strmodif = line.substring(line.indexOf(':')+1).trim();
-                                if(!strmodif.isEmpty()){
-                                    long lastmodifvalue = Long.parseLong(strmodif);
-                                    long lastModif = analyseAll(root);
-                                    if(lastmodifvalue != lastModif){
-                                        last_modified = true;
-                                    }
-                                } else {
-                                    line = "Last Modified (long) : "+analyseAll(root);
-                                    lines.add(line);
-                                }
 
-                            }
-                            if(line.contains("IndexerVolumeGuid : ")){
-                                String indexvalue = line.substring(line.indexOf(':')+1).trim();
-                                File indexFile = new File(root.getAbsolutePath()+"System Volume Information\\IndexerVolumeGuid");
-                                FileReader fileReader = new FileReader(indexFile, Charset.forName(getEncodage(indexFile)));
-                                BufferedReader reader = new BufferedReader(fileReader);
-                                String lineindex = reader.readLine();
-                                reader.close();
-                                fileReader.close();
-                                if(!indexvalue.isEmpty()){
-                                    if(indexvalue.equals(lineindex)){
-                                        index = true;
-                                    }
-                                } else {
-                                    line = "IndexerVolumeGuid : "+lineindex;
-                                    lines.add(line);
-                                }
-                            }
-                            if(line.contains("HashCode : ")){
-                                //faire un truc avec le hash code
-                            }
-                        }
-                        scan.close();
+                    //Creation du tag
+                    File tag = USBDetector.makeTAG(root);
 
-                        if(!lines.isEmpty()){
-                            FileWriter fw = new FileWriter(TAG,false);
-                            for(String str : lines){
-                                fw.write(str+"\n");
-                            }
-                            fw.close();
-                        }
+                    //Recuperation du tag crypté
+                    File crypted_tag = new File(root.getAbsolutePath()+"System Volume Information\\TAG");
 
+                    File tmp = File.createTempFile("uncrypted","",new File(System.getProperty("user.dir")));
+                    tmp.deleteOnExit();
+                    File decrypted_tag = HashUtils.decrypt(crypted_tag, tmp, "StageInformatiqu".getBytes());
 
-                        sc.nextLine();
-                        //if virus
-                        if(!index || !last_modified || !hashcode /*|| virus*/){
-                            eject(root);
+                    FileReader normalFileReader = new FileReader(tag);
+                    FileReader cryptedFileReader = new FileReader(decrypted_tag);
+
+                    BufferedReader normalReader = new BufferedReader(normalFileReader);
+                    BufferedReader cryptedReader = new BufferedReader(cryptedFileReader);
+
+                    /*String line,line2;
+                    boolean diff = false;
+                    while((((line=normalReader.readLine())!=null) && (line2=cryptedReader.readLine())!=null)) {
+                        if (!line.equals(line2)) {
+                            System.out.println("Différence détecté : \n" +
+                                    line + "\n" +
+                                    line2 + "\n");
+                            diff = true;
+                            break;
                         }
-                        i--;
-                    } catch (IOException e) {
-                        throw new RuntimeException(e);
+                    }*/
+
+                    String line1 = normalReader.readLine();
+                    String line2 = cryptedReader.readLine();
+                    System.out.println("Tag de la méthode makeTAG() : \n");
+                    while(line1 != null){
+                        System.out.println(line1);
+                        line1 = normalReader.readLine();
                     }
+
+                    System.out.println("\nTag décrypté : \n");
+                    while(line2 != null){
+                        System.out.println(line2);
+                        line2 = cryptedReader.readLine();
+                    }
+
+                    sc.nextLine();
+
+                    //if(diff){
+                        USBDetector.eject(root);
+                    //}
+
+                    tag.delete();
+
+                    i--;
                 }
             }
-            roots = newRoots;
 
-            for(String key : usb_connected.keySet()){
-                USBThread ut = new USBThread(usb_connected.get(key));
-            }
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                System.out.println(e.getMessage());
-            }
         }
-    }
-
-    public static String getEncodage(File file) throws IOException {
-        String res = "";
-        FileInputStream fis = new FileInputStream(file);
-        byte[] data = new byte[(int) file.length()];
-        fis.read(data);
-        fis.close();
-        CharsetDetector detector = new CharsetDetector();
-        detector.setText(data);
-        CharsetMatch[] matches = detector.detectAll();
-        if(matches.length > 0){
-            res = matches[0].getName();
-        }
-        return res;
-    }
-
-    private static long analyseAll(File file){
-        long max_modif = 0;
-        for(File f : file.listFiles()){
-            if(!f.getName().equals("System Volume Information")){
-                if(f.isDirectory()){
-                    long tmp = analyseAll(f);
-                    if(max_modif<tmp){
-                        max_modif = tmp;
-                    }
-                } else {
-                    if(max_modif<f.lastModified()){
-                        max_modif = f.lastModified();
-                    }
-                }
-            }
-        }
-        return max_modif;
-    }
-
-    private static void eject(File drive) throws IOException {
-        // Récupérer la lettre de lecteur de la clé USB
-        String driveLetter = drive.getAbsolutePath().substring(0,1);
-
-        String batchFilePath = System.getProperty("user.dir") + "\\src\\v2\\ejecter.cmd";
-        Process process = Runtime.getRuntime().exec(" "+batchFilePath+" "+driveLetter+" "+System.getProperty("user.dir"));
-
-        BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
-        String line;
-        StringBuilder output = new StringBuilder();
-        while ((line = reader.readLine()) != null) {
-            output.append(line + "\n");
-        }
-        System.out.println(output);
     }
 }
