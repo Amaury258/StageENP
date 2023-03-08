f3e1415efdf85e4f95d9e65a59e4131bc61f133c
Amaury258 <amaury.demarque3@etu.univ-lorraine.fr>
2023-03-08 15:23:38 +0100

    Modification du Main qui ici vas creer un tag, le chiffrer et ejecter la clé usb

--- src/v2/Main.java
+++ src/v2/Main.java
@@ -7,78 +7,88 @@
 
 package v2;
 
+import org.apache.commons.io.IOUtils;
+
 import java.io.*;
+import java.nio.channels.FileChannel;
+import java.nio.channels.FileLock;
+import java.nio.charset.Charset;
 import java.nio.file.*;
 import java.util.*;
 
 import java.nio.channels.ClosedChannelException;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.stream.Collectors;
+import javax.crypto.spec.SecretKeySpec;
 import javax.swing.filechooser.FileSystemView;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        FileSystemView fsv = FileSystemView.getFileSystemView();
-        File[] drives = File.listRoots();
-        HashMap<String,File> usbDrives = new HashMap<String,File>();
-        if (drives != null && drives.length > 0) {
-            for (File aDrive : drives) {
-                if(fsv.getSystemTypeDescription(aDrive).contains("USB")){
-                    //analyse antivirus + autorisation
-                    System.out.println("Clé USB détectée : " + aDrive);
-                    ejectUSB(aDrive.toPath());
+        int i = 0;
+        while(true){
+            if (i == 0) {
+                System.out.println("Recherche de clé USB");
+                i++;
+            }
+
+            Scanner sc = new Scanner(System.in);
+            FileSystemView fsv = FileSystemView.getFileSystemView();
+            File[] newRoots = File.listRoots();
+            //parcours des disques
+            for (File root : newRoots) {
+                if (fsv.isDrive(root) && fsv.getSystemTypeDescription(root).contains("USB")){
+                    // la clé USB a été trouvée, faites quelque chose ici
+                    System.out.println("Clé USB détecté : " + root.getAbsolutePath());
+
+                    //Creation du tag
+                    File tag = USBDetector.makeTAG(root);
+                    FileReader fileReader = new FileReader(tag);
+                    BufferedReader reader = new BufferedReader(fileReader);
+
+                    //Lecture du tag pour vérifier qu'il y a bien ce qu'on veux
+                    String line = reader.readLine();
+                    while(line != null){
+                        System.out.println(line);
+                        line = reader.readLine();
+                    }
+
+                    reader.close();
+                    fileReader.close();
+
+
+                    //Chiffrement du tag + sauvegarde sur la clé usb
+                    File CRYPTED_TAG = HashUtils.encrypt(tag, root.getAbsolutePath()+"System Volume Information\\TAG",
+                            "StageInformatiqu".getBytes());
+
+
+
+                    System.out.println("\nFichier crypté\n");
+
+                    tag.delete();
+
+                    //Lecture du fichier chiffré pour s'assurer qu'il est chiffré
+                    fileReader = new FileReader(CRYPTED_TAG);
+                    reader = new BufferedReader(fileReader);
+                    line = reader.readLine();
+                    while(line != null){
+                        System.out.println(line);
+                        line = reader.readLine();
+                    }
+
+                    reader.close();
+                    fileReader.close();
+
+                    sc.nextLine();
+
+                    //Ejection de la clé usb
+                    USBDetector.eject(root);
+                    i--;
                 }
-                /*if(isRemovableDrive(aDrive.toPath())){
-                    //analyse antivirus + autorisation
-                    System.out.println("Clé USB détectée : " + aDrive);
-                    //ejectUSB(aDrive);
-                }*/
             }
+
         }
-
-        /*FileSystemView fileSystemView = FileSystemView.getFileSystemView();
-        File[] roots = File.listRoots();
-        
-        for (File root : roots) {
-            String type = fileSystemView.getSystemTypeDescription(root);
-            boolean isRemovable = fileSystemView.isDrive(root) && fileSystemView.isFloppyDrive(root) && !fileSystemView.isFileSystemRoot(root) && fileSystemView.getSystemTypeDescription(root).contains("Removable");
-            
-            if (isRemovable) {
-                // Le disque est amovible (clé USB)
-                System.out.println("Clé USB détectée : " + root);
-                
-                // Éjecte la clé USB
-                ejectUSB(root);
-            } else {
-                // Le disque n'est pas amovible (disque dur interne)
-                System.out.println("Disque dur détecté : " + root);
-            }
-        }*/
-
-        /*if(usbDrives.isEmpty()){
-            System.out.println("Aucun périphérique USB détecté");
-        } else {
-            for (Map.Entry<String, File> entry : usbDrives.entrySet()) {
-                System.out.println("Périphérique USB détecté : " + entry.getKey());
-                //analyse antivirus + autorisation
-                ejectUSB(entry.getKey());
-            }
-        }*/
-    }
-
-    private static boolean isRemovableDrive(Path rootDirectory) {
-        File file = rootDirectory.toFile();
-        return file.canRead() && file.canWrite() && file.isDirectory() && file.listFiles() != null && file.listFiles().length > 0;
-    }
-
-    private static void ejectUSB(Path path) throws Exception{
-        String command = "devcon remove \"" + path.toString() + "\"";
-        Process process = Runtime.getRuntime().exec(command);
-        Scanner scanner = new Scanner(process.getInputStream());
-
-        while (scanner.hasNext()) {
-            System.out.println(scanner.nextLine());
-        }
-
-        process.waitFor();
-        System.out.println("Clé USB " + path.toString() + " éjectée.");
     }
 }
